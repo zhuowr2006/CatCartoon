859201d493d6889cfb9a1807fd088db57aaf67f6
zhuowr2006 <251592818@qq.com>
2017-11-08 09:44:50 +0800

    搜索列表界面完成

--- app/src/main/java/com/homa/catcartoon/ui/search/SearchActivity.java
+++ app/src/main/java/com/homa/catcartoon/ui/search/SearchActivity.java
@@ -3,6 +3,7 @@ package com.homa.catcartoon.ui.search;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -17,6 +18,7 @@ import com.homa.catcartoon.net.HttpApiManager;
 import com.homa.catcartoon.ui.List.bean.ListBean;
 import com.homa.catcartoon.utils.ImageLoaderUtil;
 import com.litesuits.android.log.Log;
+import com.litesuits.common.assist.Check;
 import com.wzgiceman.rxretrofitlibrary.retrofit_rx.exception.ApiException;
 import com.wzgiceman.rxretrofitlibrary.retrofit_rx.http.HttpManager;
 
@@ -46,6 +48,7 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
     private HttpManager httpManager;
     private String nextPageUrl;//下一页的url
     private String content;
+
     @Override
     public int getLayoutResId() {
         return R.layout.fragment_recom;
@@ -56,9 +59,10 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
         list = new ArrayList<>();
         swipeLayout.setOnRefreshListener(this);
         swipeLayout.setColorSchemeColors(getResources().getColor(R.color.app_color));
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        GridLayoutManager layoutManager=new GridLayoutManager(this, 2);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         fanjuRcview.setLayoutManager(layoutManager);
-        adapter = new SubareaAdapter(R.layout.item_home_list, list);
+        adapter = new SubareaAdapter(R.layout.item_home_search, list);
         adapter.openLoadAnimation();
         // 滑动最后一个Item的时候回调onLoadMoreRequested方法
         adapter.setOnLoadMoreListener(this);
@@ -81,24 +85,31 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
 //                }
             }
         });
-        content=getIntent().getStringExtra("content");
+        content = getIntent().getStringExtra("content");
         httpManager = new HttpManager(this, this);
-        HttpApiManager.getSearch(httpManager,content);
+        HttpApiManager.getSearch(httpManager, content);
         swipeLayout.setRefreshing(true);
     }
+
     @Override
-    public void onNext(String resulte, String method) {
+    public void onNext(final String resulte, String method) {
         if (swipeLayout.isRefreshing()) {//刷新
             swipeLayout.setRefreshing(false);
             getData(resulte, list);
             adapter.setNewData(list);
-        }else {
-            //加载更多
-            //成功获取更多数据
-            List<ListBean> add = new ArrayList<>();
-            getData(resulte, add);
-            adapter.addData(add);
-            adapter.loadMoreComplete();
+        } else {
+//            //成功获取更多数据
+            fanjuRcview.postDelayed(new Runnable() {//必须要延时，不然addData()之后立刻调用loadMoreComplete会触发加载更多，从而无线循环去请求加载更多数据
+                @Override
+                public void run() {
+                    List<ListBean> add = new ArrayList<>();
+                    getData(resulte, add);
+                    adapter.addData(add);
+                    adapter.loadMoreComplete();
+                }
+
+            }, 1000);
+
         }
 
 
@@ -106,7 +117,7 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
 
     @Override
     public void onError(ApiException e, String method) {
-        if (swipeLayout.isRefreshing()){//刷新
+        if (swipeLayout.isRefreshing()) {//刷新
             Toast.makeText(this, "刷新失败", Toast.LENGTH_LONG).show();
             swipeLayout.setRefreshing(false);
         } else {
@@ -120,7 +131,6 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
         try {
             //从一个URL加载一个Document对象。
             Document doc = Jsoup.parse(resulte);
-            System.out.println("ttttt");
             for (Element e : doc.select("div.item")) {
 //                Log.i("排行数据",e.select("span").text()+e.select("a").attr("href")+e.select("a").attr("title"));
                 ListBean listBean = new ListBean();
@@ -130,15 +140,20 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
 //                listBean.setSummary(e.select("div.info").select("p.line").select("span.ibcont").text());
                 listBean.setTitle(e.select("img").attr("title"));
                 listBean.setTip(e.select("p.tip").select("a").text());
-//                data.add(listBean);
+                data.add(listBean);
 //                System.out.println("=="+e.toString());
 //                System.out.println("=="+e.select("img").attr("src"));
 //                System.out.println("=="+e.select("img").attr("title"));
 //                System.out.println("=="+e.select("p.tip").select("a").text());
             }
 //            System.out.println("=="+);
-            String url=doc.select("div.page").select("a.next").attr("href");
-//            nextPageUrl=""//去掉前面那部分只留后面那部分，就ok了
+            String url = doc.select("div.page").select("a.next").attr("href");
+            if (!Check.isEmpty(url)){
+                nextPageUrl = content + url.substring(url.lastIndexOf("/") - 2, url.length());//去掉前面那部分只留后面那部分，就ok了
+                System.out.println(nextPageUrl);
+            }else {
+                nextPageUrl=url;
+            }
         } catch (Exception e) {
             Log.i("mytag", e.toString());
         }
@@ -148,12 +163,18 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
 
     @Override
     public void onLoadMoreRequested() {
-        HttpApiManager.getSearch(httpManager,nextPageUrl);
+        if (!Check.isEmpty(nextPageUrl)){
+            HttpApiManager.getSearch(httpManager, nextPageUrl);
+        }else {
+            //数据全部加载完毕
+            adapter.loadMoreEnd();
+        }
     }
+
 
     @Override
     public void onRefresh() {
-        HttpApiManager.getSearch(httpManager,"/search-"+content);
+        HttpApiManager.getSearch(httpManager, "/search-" + content);
     }
 
     public class SubareaAdapter extends BaseQuickAdapter<ListBean, BaseViewHolder> {
@@ -165,11 +186,11 @@ public class SearchActivity extends BaseActivity implements SwipeRefreshLayout.O
 
         @Override
         protected void convert(BaseViewHolder helper, ListBean item) {
-            helper.setText(R.id.item_list_num, item.getTip());
-            helper.setText(R.id.item_list_title, item.getTitle());
-            helper.setText(R.id.item_list_zuoze, item.getName());
-            helper.setText(R.id.item_list_jianjie, item.getSummary());
-            ImageView img = helper.getView(R.id.item_list_img);
+            //隐藏
+
+            helper.setText(R.id.item_srarch_title, item.getTitle());
+            helper.setText(R.id.item_srarch_jianjie, item.getTip());
+            ImageView img = helper.getView(R.id.item_srarch_img);
             ImageLoaderUtil.loader(SearchActivity.this, item.getImg(), img);
 
         }
